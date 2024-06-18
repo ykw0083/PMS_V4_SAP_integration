6778ac2b82ce2ad856599d090f2a30d67f8d6464
ykw <ykw@myfastrack.net>
2024-06-18 22:52:19 +0800

    add single instance.

--- PMS_V4_SAP_integration/Program.cs
+++ PMS_V4_SAP_integration/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,10 +16,22 @@ namespace PMS_V4_SAP_integration
         [STAThread]
         static void Main(string []args)
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1(args));
+            string procName = Process.GetCurrentProcess().ProcessName;
+            // get the list of all processes by that name
+
+            Process[] processes = Process.GetProcessesByName(procName);
+
+            if (processes.Length > 1)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1(args));
+            }
         }
     }
 }
